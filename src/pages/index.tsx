be51e7256cd69f29046e8cd247ca8195acc61821
shulandmimi <sshuang141@163.com>
2022-03-26 15:38:17 +0800

    feat: 添加指标记录

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { UseRequestProvider } from 'ahooks';
 import { store } from '@/store';
 import App from './App';
 import './index.scss';
+import '@/tools/ga';
 
 // if ('serviceWorker' in navigator) {
 //     // Use the window load event to keep the page load performant
